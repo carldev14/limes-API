9cf00a6398656dd503a40d0ea962fa388b98c8cd
carldev14 <carldev.ac@gmail.com>
2024-11-08 20:49:27 +0800

    try

--- src/utils/gen-token-and-set-cookie.ts
+++ src/utils/gen-token-and-set-cookie.ts
@@ -13,7 +13,7 @@ export const generateTokenAndSetCookie = (res: Response, userId: string) => {
 		secure: NODE_ENV === "production",
 		sameSite: "strict",
 		maxAge: 7 * 24 * 60 * 60 * 1000,
-		domain: 'https://limes-express-backend.onrender.com'
+		domain: 'https://limes-apis.onrender.com'
 	});
 
 	return token;
